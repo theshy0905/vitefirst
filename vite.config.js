968adf92f627c6bcfe5a9a3a0471ed6883300d65
沈荻 <2951546725@qq.com>
2023-03-01 10:58:39 +0800

    测试

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve:{
+    alias: {
+       '@':fileURLToPath(new URL('./src',import.meta.url)),
+       '@v':fileURLToPath(new URL('./src/views',import.meta.url)),
+       '@c':fileURLToPath(new URL('./src/components',import.meta.url))
+    }
+  }
 })
